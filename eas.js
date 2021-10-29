f4ef90af8679d82e1353443a705fa329aa310638
Ollie <olliecrawford42@gmail.com>
2021-10-29 14:45:24 +0100

    First commit

---
+++ eas.js
@@ -0,0 +1,45 @@
+let audio = new Audio('./sounds/shakesound.wav');
+
+function restart() {
+    audio.play();
+    const etchArea = document.querySelector('#etchArea')
+    function removeAllChildNodes(parent) {
+        while (parent.firstChild) {
+            parent.removeChild(parent.firstChild);
+        }
+    }
+    removeAllChildNodes(etchArea)
+    start()
+}
+
+function start() {
+    let num = prompt("What would you like the width/height to be?")
+
+    let i = (num * num - 1);
+    const etchArea = document.querySelector('#etchArea');
+
+    document.getElementById("etchArea").style["grid-template-columns"] = "repeat(" + num + ", 1fr)";
+    document.getElementById("etchArea").style["grid-template-rows"] = "repeat(" + num + ", 1fr)";
+
+    for (i; i > -1; i--) {
+        let square = document.createElement('div');
+        square.classList.add('square');
+        square.setAttribute("id", "square" + i);
+        etchArea.appendChild(square);
+        square.addEventListener('mouseenter', (e) => {
+            if (square.classList.contains("mouseover")) {
+                square.classList.remove("mouseover");
+                square.classList.add("mouseoff")
+            }
+            else {
+                square.classList.remove("mouseoff")
+                square.setAttribute("class", "mouseover");
+            }
+        })
+    }
+}
+start();
+
+const shake = document.getElementById('shake').onclick = restart;
+
+
