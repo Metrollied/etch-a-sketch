cb5e14ae23952c8806d705764ffbbda4ebc37569
Ollie <olliecrawford42@gmail.com>
2021-10-30 09:02:35 +0100

    Stopped user from being able to add anything but integer numbers.

--- eas.js
+++ eas.js
@@ -15,27 +15,32 @@ function restart() {
 function start() {
     let num = prompt("What would you like the width/height to be?")
 
-    let i = (num * num - 1);
-    const etchArea = document.querySelector('#etchArea');
+    if (num > 0 && Number.isInteger(num)) {
+        let i = (num * num - 1);
+        const etchArea = document.querySelector('#etchArea');
 
-    document.getElementById("etchArea").style["grid-template-columns"] = "repeat(" + num + ", 1fr)";
-    document.getElementById("etchArea").style["grid-template-rows"] = "repeat(" + num + ", 1fr)";
+        document.getElementById("etchArea").style["grid-template-columns"] = "repeat(" + num + ", 1fr)";
+        document.getElementById("etchArea").style["grid-template-rows"] = "repeat(" + num + ", 1fr)";
 
-    for (i; i > -1; i--) {
-        let square = document.createElement('div');
-        square.classList.add('square');
-        square.setAttribute("id", "square" + i);
-        etchArea.appendChild(square);
-        square.addEventListener('mouseenter', (e) => {
-            if (square.classList.contains("mouseover")) {
-                square.classList.remove("mouseover");
-                square.classList.add("mouseoff")
-            }
-            else {
-                square.classList.remove("mouseoff")
-                square.setAttribute("class", "mouseover");
-            }
-        })
+        for (i; i > -1; i--) {
+            let square = document.createElement('div');
+            square.classList.add('square');
+            square.setAttribute("id", "square" + i);
+            etchArea.appendChild(square);
+            square.addEventListener('mouseenter', (e) => {
+                if (square.classList.contains("mouseover")) {
+                    square.classList.remove("mouseover");
+                    square.classList.add("mouseoff")
+                }
+                else {
+                    square.classList.remove("mouseoff")
+                    square.setAttribute("class", "mouseover");
+                }
+            })
+        }
+    }
+    else {
+        alert('You need to enter a whole number above 0. Press \'shake\' to restart.');
     }
 }
 start();
